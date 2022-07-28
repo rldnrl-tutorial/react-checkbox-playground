ba76856c6d8cfc1afc8e0bbc357874ee2454370d
rldnrl <devpark1002@gmail.com>
2022-07-28 22:08:02 +0900

    feat: allAgreements 체크 여부에 대한 로직 구현

--- src/compound-components/Agreements.tsx
+++ src/compound-components/Agreements.tsx
@@ -1,4 +1,11 @@
-import { ChangeEvent, ReactNode, useCallback, useState } from "react";
+import {
+  ChangeEvent,
+  ReactNode,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useImmerReducer } from "use-immer";
 
 import AgreementsContext, {
@@ -13,9 +20,21 @@ type AgreementsProps = {
 };
 
 export default function Agreements({ children }: AgreementsProps) {
+  const cachedRequiredField = useRef<Set<TermValue>>(new Set<TermValue>());
   const [requiredField, setRequiredField] = useState<Set<TermValue>>(
     new Set<TermValue>()
   );
+
+  const initializeRequiredField = (name: TermValue, required = false) => {
+    if (required) {
+      setRequiredField((prevSet) => {
+        const newSet = new Set<TermValue>(prevSet);
+        newSet.add(name);
+        return newSet;
+      });
+      cachedRequiredField.current.add(name);
+    }
+  };
 
   const [state, dispatch] = useImmerReducer(
     agreementsReducer,
@@ -23,11 +42,50 @@ export default function Agreements({ children }: AgreementsProps) {
   );
 
   const changeTermCheck = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) =>
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const agreementName = e.target.name as AgreementsAction["type"];
+
+      const isCheckedRequiredField = e.target.required && e.target.checked;
+      const isUncheckedRequiredField = e.target.required && !e.target.checked;
+
+      if (isCheckedRequiredField) {
+        setRequiredField((prevRequiredField) => {
+          const newSet = new Set(prevRequiredField);
+          newSet.delete(agreementName as TermValue);
+          return newSet;
+        });
+      }
+
+      if (isUncheckedRequiredField) {
+        setRequiredField((prevRequiredField) => {
+          const newSet = new Set(prevRequiredField);
+          newSet.add(agreementName as TermValue);
+          return newSet;
+        });
+      }
+
+      const isCheckedAllAgreements =
+        agreementName === "allAgreements" && e.target.checked;
+      const isUnCheckedAllAgreements =
+        agreementName === "allAgreements" && !e.target.checked;
+
+      if (isCheckedAllAgreements) {
+        setRequiredField((prevRequiredField) => {
+          const newSet = new Set(prevRequiredField);
+          newSet.clear();
+          return newSet;
+        });
+      }
+
+      if (isUnCheckedAllAgreements) {
+        setRequiredField(cachedRequiredField.current);
+      }
+
       dispatch({
         type: e.target.name as AgreementsAction["type"],
         payload: e.target.checked,
-      }),
+      });
+    },
     [dispatch]
   );
 
@@ -43,14 +101,22 @@ export default function Agreements({ children }: AgreementsProps) {
 
   const reset = useCallback(() => dispatch({ type: "reset" }), [dispatch]);
 
+  const isCheckedAllRequiredField = useMemo(
+    () => requiredField.size === 0,
+    [requiredField]
+  );
+
   return (
     <AgreementsContext.Provider
       value={{
         agreements: state,
+        cachedRequiredField,
         requiredField,
         setRequiredField,
+        initializeRequiredField,
         isAllChecked,
         changeTermCheck,
+        isCheckedAllRequiredField,
         reset,
       }}
     >
